56f7b607a67c8aaada5199aea694c3d3e069100d
91834 <91834@BABLU>
2023-12-24 01:33:15 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace SpreadSheetSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.cbColumn = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cbSheet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnChoseFile = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSplit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSplit.Location = new System.Drawing.Point(78, 258);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(115, 31);
+            this.btnSplit.TabIndex = 3;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // cbColumn
+            // 
+            this.cbColumn.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbColumn.FormattingEnabled = true;
+            this.cbColumn.Location = new System.Drawing.Point(12, 153);
+            this.cbColumn.Name = "cbColumn";
+            this.cbColumn.Size = new System.Drawing.Size(368, 25);
+            this.cbColumn.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(199, 258);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(115, 31);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cbSheet
+            // 
+            this.cbSheet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSheet.FormattingEnabled = true;
+            this.cbSheet.Location = new System.Drawing.Point(12, 95);
+            this.cbSheet.Name = "cbSheet";
+            this.cbSheet.Size = new System.Drawing.Size(368, 25);
+            this.cbSheet.TabIndex = 2;
+            this.cbSheet.SelectedIndexChanged += new System.EventHandler(this.cbSheet_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Sheet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Column With Date";
+            // 
+            // btnChoseFile
+            // 
+            this.btnChoseFile.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.btnChoseFile.Location = new System.Drawing.Point(12, 34);
+            this.btnChoseFile.Name = "btnChoseFile";
+            this.btnChoseFile.Size = new System.Drawing.Size(368, 27);
+            this.btnChoseFile.TabIndex = 5;
+            this.btnChoseFile.Text = "Chose File";
+            this.btnChoseFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnChoseFile.UseVisualStyleBackColor = true;
+            this.btnChoseFile.Click += new System.EventHandler(this.btnChoseFile_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Select Excel File";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 214);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(368, 19);
+            this.progressBar.TabIndex = 6;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(12, 194);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(0, 17);
+            this.lblProgress.TabIndex = 4;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSplit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 301);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnChoseFile);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSheet);
+            this.Controls.Add(this.cbColumn);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSplit);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spreadsheet Splitter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.ComboBox cbColumn;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cbSheet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnChoseFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblProgress;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace SpreadSheetSplitter
+{
+    public partial class Form1 : Form
+    {
+        public string filePath = "";
+        public string exportPath = "";
+        public string worksheet = "";
+        public int columnIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
+        private void btnChoseFile_Click(object sender, EventArgs e)
+        {
+            Color tempColur = btnChoseFile.ForeColor;
+            string tempTxt = btnChoseFile.Text;
+            btnChoseFile.ForeColor = SystemColors.WindowText;
+            btnChoseFile.Text = string.Empty;
+
+
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                cbSheet.Items.Clear();
+
+                dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                dialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm|All Files|*.*";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = dialog.FileName;
+                    btnChoseFile.Text = Path.GetFileName(filePath);
+
+                    //add sheet names to combobox
+                    List<string> sheets = GetSheetNames(filePath);
+                    foreach (string sheet in sheets)
+                    {
+                        cbSheet.Items.Add(sheet);
+                    }
+                }
+                else
+                {
+                    btnChoseFile.Text = tempTxt;
+                    btnChoseFile.ForeColor = tempColur;
+                }
+
+                cbColumn.Focus();
+            }
+        }
+
+        private void cbSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbColumn.Items.Clear();
+
+            List<string> colList = GetColumnNames(filePath, cbSheet.Text);
+            foreach (string col in colList)
+            {
+                cbColumn.Items.Add(col);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker.IsBusy)
+                backgroundWorker.CancelAsync();
+
+            btnChoseFile.Text = "Chose File";
+            btnChoseFile.ForeColor = Color.Gray;
+
+            cbSheet.Items.Clear();
+            cbColumn.Items.Clear();
+
+            filePath = string.Empty;
+            exportPath = string.Empty;
+            worksheet = string.Empty;
+            columnIndex = -1;
+
+            progressBar.Value = 0;
+            btnSplit.Enabled = true;
+            lblProgress.Text = string.Empty;
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                //change variables
+                worksheet = cbSheet.Text;
+                columnIndex = cbColumn.SelectedIndex + 1;
+                using (FolderBrowserDialog d = new FolderBrowserDialog())
+                {
+                    if (filePath == string.Empty)
+                        return;
+
+                    string fileName = Path.GetFileName(filePath);
+                    d.SelectedPath = filePath.Substring(0, filePath.Length - fileName.Length);
+                    if (d.ShowDialog() == DialogResult.OK)
+                    {
+                        exportPath = d.SelectedPath;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Description : Select a valid export path", "Btn-Split-SelectFolderDialog", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+                btnSplit.Enabled = false;
+
+
+
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+
+
+        //background worker
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<string> distinctValues = GetDistinctColumnValues(filePath, worksheet, columnIndex);
+
+            int tempProgress = 0;
+            foreach (string value in distinctValues)
+            {
+                List<int> rowIndexsRead = GetMatchingRowIndexes(filePath, worksheet, columnIndex, value);
+
+                if (rowIndexsRead.Any())
+                {
+                    var packageWrite = new ExcelPackage();
+
+                    ExcelWorksheet worksheetWrite = packageWrite.Workbook.Worksheets.Add("Sheet1");
+
+                    ExcelPackage packageRead = new ExcelPackage(new FileInfo(filePath));
+
+                    ExcelWorksheet worksheetRead = packageRead.Workbook.Worksheets[worksheet];
+                    List<int> colIndexesRead = new List<int>();
+                    for (int i = 1; i <= worksheetRead.Dimension.End.Column; i++)
+                    {
+                        if (i != columnIndex)
+                            colIndexesRead.Add(i);
+                    }
+
+                    worksheetWrite.Cells[1, 1].Value = "Variables";
+                    worksheetWrite.Cells[1, 1].Style.Font.Bold = true;
+                    worksheetWrite.Cells[2, 1].Value = "PAYMENT DATE RANGE";
+                    worksheetWrite.Cells[2, 2].Value = $"{value} - {value}";
+
+                    int colIndexWrite = 1;
+                    foreach (int colIndexRead in colIndexesRead)
+                    {
+                        //set column names
+                        worksheetWrite.Cells[4, colIndexWrite].Value = worksheetRead.Cells[1, colIndexRead].Value;
+                        worksheetWrite.Cells[4, colIndexWrite].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                        worksheetWrite.Cells[4, colIndexWrite].Style.Fill.BackgroundColor.SetColor(Color.LightGray);
+                        worksheetWrite.Cells[4, colIndexWrite].AutoFitColumns();
+                        //set table values
+                        int rowIndexWrite = 5;
+                        foreach (int rowIndexRead in rowIndexsRead)
+                        {
+                            //cancel background worker
+                            if (backgroundWorker.CancellationPending)
+                            {
+                                backgroundWorker.Dispose();
+                            }
+
+                            if (rowIndexWrite != rowIndexsRead.Count+4)
+                            {
+                                worksheetWrite.Cells[rowIndexWrite, 1].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                                worksheetWrite.Cells[rowIndexWrite, 1].Style.Fill.BackgroundColor.SetColor(Color.LightGray);
+                            }
+
+                            worksheetWrite.Cells[rowIndexWrite, colIndexWrite].Value = worksheetRead.Cells[rowIndexRead, colIndexRead].Value;
+                            rowIndexWrite++;
+                        }
+                        worksheetWrite.Cells[rowIndexsRead.Count + 4, colIndexWrite].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                        worksheetWrite.Cells[rowIndexsRead.Count + 4, colIndexWrite].Style.Fill.BackgroundColor.SetColor(Color.Yellow);
+
+                        colIndexWrite++;
+                    }
+                    string fileExportPath = Path.Combine(exportPath, $"{value}.xlsx");
+                    packageWrite.SaveAs(new FileInfo(fileExportPath));
+                }
+                tempProgress++;
+
+                System.Threading.Thread.Sleep(100);
+                int progressPercentage = (int)((double)tempProgress / distinctValues.Count * 100);
+                backgroundWorker.ReportProgress(progressPercentage);
+
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            lblProgress.Text = $"Almost there {e.ProgressPercentage}% Done";
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show($"Saved Into :\n\n{exportPath}", "Saved Successfuly", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        //functions
+        public List<string> GetSheetNames(string filePath)
+        {
+            List<string> result = new List<string>();
+
+            try
+            {
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    foreach (ExcelWorksheet excelWorksheet in package.Workbook.Worksheets)
+                    {
+                        result.Add(excelWorksheet.Name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Description : {ex.Message}", "Error in GetSheetNames()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+
+        public List<string> GetColumnNames(string filePath, string sheetName)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorkbook workbook = package.Workbook;
+                    ExcelWorksheet worksheet = workbook.Worksheets[sheetName];
+
+                    for (int i = 1; i < worksheet.Dimension.End.Column; i++)
+                    {
+                        result.Add(worksheet.Cells[1, i].Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Description : {ex.Message}", "Error in GetColumnNames()", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return result;
+        }
+
+        public static List<string> GetDistinctColumnValues(string filePath, string sheetName, int colIndex)
+        {
+            List<string> distinctValues = new List<string>();
+
+            // Load the Excel file
+            FileInfo fileInfo = new FileInfo(filePath);
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                // Get the worksheet
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetName];
+
+
+                // Iterate through the rows to get distinct values
+                for (int row = 2; row < worksheet.Dimension.End.Row-2; row++)
+                {
+                    var cellValue = worksheet.Cells[row, colIndex].Text;
+                    if (!distinctValues.Contains(cellValue))
+                    {
+                        distinctValues.Add(cellValue);
+                    }
+                }
+            }
+
+            return distinctValues;
+        }
+
+        public static List<int> GetMatchingRowIndexes(string filePath, string sheetName, int colIndex, string matchValue)
+        {
+            List<int> matchingIndexes = new List<int>();
+
+            // Load the Excel file
+            FileInfo fileInfo = new FileInfo(filePath);
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                // Get the worksheet
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetName];
+
+
+                // Iterate through the rows to find matching values
+                for (int row = 2; row <= worksheet.Dimension.End.Row-1; row++)
+                {
+                    var cellValue = worksheet.Cells[row, colIndex].Text;
+                    if (cellValue.Equals(matchValue, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchingIndexes.Add(row);
+                    }
+                }
+            }
+
+            return matchingIndexes;
+        }
+
+    }
+}
